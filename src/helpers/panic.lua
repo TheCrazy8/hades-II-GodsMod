7e567b2200abadac93e30b5dccf4e45e4992582b
Scarlett/Eight <bennettbashford@outlook.com>
2026-06-08 12:56:14 -0400

    Update panic.lua

--- src/helpers/panic.lua
+++ src/helpers/panic.lua
@@ -1,4 +1,7 @@
+-- helpers/Panic.lua
+
 TC8GodMod = TC8GodMod or {}
+
 TC8GodMod.PanickedEnemies = {}
 
 function TC8GodMod.TryApplyPanic(triggerArgs)
@@ -12,10 +15,25 @@ function TC8GodMod.TryApplyPanic(triggerArgs)
         return
     end
 
+    -- Don't reapply if already panicked
+    if TC8GodMod.PanickedEnemies[victim.ObjectId] ~= nil then
+        return
+    end
+
     TC8GodMod.PanickedEnemies[victim.ObjectId] = {
         TargetEnemies = true,
         ExpireTime = _worldTime + 4
     }
 
-    print("[TC8GodMod] Panic applied to " .. tostring(victim.ObjectId))
+    -- Allow this enemy to receive damage from other enemies
+    AddIncomingDamageModifier(victim, {
+        Name = "TC8GodMod_PanicFriendlyFire",
+        NonPlayerMultiplier = 1,
+        Multiplicative = true
+    })
+
+    print(
+        "[TC8GodMod] Panic applied to " ..
+        tostring(victim.ObjectId)
+    )
 end
