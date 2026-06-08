3317e15cb09c1c282c2f5ff9354fd448610a7bba
Scarlett/Eight <bennettbashford@outlook.com>
2026-06-08 12:38:50 -0400

    Update panic.lua

--- src/helpers/panic.lua
+++ src/helpers/panic.lua
@@ -1,5 +1,24 @@
 TC8GodMod = TC8GodMod or {}
 
+TC8GodMod = TC8GodMod or {}
+TC8GodMod.PanickedEnemies = {}
+
+function TC8GodMod.TryApplyPanic(triggerArgs)
+    if triggerArgs == nil then
+        return
+    end
+
+    local victim = triggerArgs.TriggeredByTable
+
+    if victim == nil then
+        return
+    end
+
+    TC8GodMod.PanickedEnemies[victim.ObjectId] = true
+
+    print("[TC8GodMod] Panic applied")
+end
+
 function TC8GodMod.TryApplyPanic(triggerArgs)
     print("[TC8GodMod] Panic hook fired")
 end
